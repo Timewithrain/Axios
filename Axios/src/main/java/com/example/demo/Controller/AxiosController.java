4695c46932ce1be0e3b32cf6e4a12f253ccc3d20
Watermelon <1050364782@qq.com>
2020-03-18 22:45:55 +0800

    Axios project v1.0.2 des:axios back-end is complete

--- Axios/src/main/java/com/example/demo/Controller/AxiosController.java
+++ Axios/src/main/java/com/example/demo/Controller/AxiosController.java
@@ -1,29 +1,50 @@
 package com.example.demo.Controller;
 
 import com.example.demo.entity.User;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/axios")
 @CrossOrigin
 public class AxiosController {
 
-    @GetMapping("/list")
-    public List<User> axios(){
+    @GetMapping("/get")
+    public List<User> get(String username,Integer age) throws InterruptedException {
+        System.out.println("name:"+username);
+        System.out.println("age:"+age);
+        Thread.sleep(3000);
         List<User> list = new ArrayList<>();
         list.add(new User(1l,"大娃",19));
         list.add(new User(2l,"二娃",19));
         list.add(new User(3l,"三娃",19));
         list.add(new User(5l,"四娃",19));
         list.add(new User(4l,"五娃",19));
+//        throw new RuntimeException("出错了");
         return list;
     }
 
+    @PostMapping("/postStr")
+    public Map<String,String> postStr(String username,Integer age){
+        System.out.println(username);
+        System.out.println(age);
+        Map<String,String> map = new HashMap<>();
+        map.put("success","true");
+        map.put("message","请求成功！");
+        return map;
+    }
+
+    @PostMapping("/post")
+    public Map<String,String> post(@RequestBody User user){
+        System.out.println("user = " + user);
+        Map<String,String> map = new HashMap<>();
+        map.put("success","true");
+        map.put("message","请求成功！");
+        return map;
+    }
 
 }
